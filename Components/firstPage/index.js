740af67dcb875fbff23fcd870e8752799694068b
Alex Shtokhov <alexeyshtokhov29@gmail.com>
2024-10-20 10:48:41 +0400

    first commit

---
+++ Components/firstPage/index.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {Text, View } from 'react-native';
+import styles from './firstPageStyles';
+
+const  FirstPage = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ghbdasd asda1!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+export default FirstPage;
